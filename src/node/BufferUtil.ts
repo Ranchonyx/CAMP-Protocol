343b9544195453166a70b612825ab190a7580eee
Ranchonyx <felixjanetzki@gmail.com>
2026-05-25 18:28:57 +0200

    fixed major fuckups for the web&node version of BufferUtil

--- src/node/BufferUtil.ts
+++ src/node/BufferUtil.ts
@@ -1,41 +1,61 @@
-import {BinaryMessageType} from "../protocol_base.js";
+import {BinaryMessageType, BufferEncoding} from "../protocol_base.js";
 
 export class BufferUtil {
     public static GetType(message: Buffer): BinaryMessageType {
-        const type = message.readUint8(16);
-        if (type > 0xff)
-            throw new Error(`Unable to decode type from message ${message}. MAX_TYPE = 255, got ${type} !`);
-
-        return type;
+        return message.readUint8(8);
     }
 
     public static GetAck(message: Buffer): number {
-        return message.readUint32BE(17);
+        return message.readUInt32BE(9);
     }
 
     public static GetSid(message: Buffer): bigint {
         return message.readBigUInt64BE(0);
     }
 
-    public static GetPayload(message: Buffer, encoding: BufferEncoding = "utf8"): string {
-        return message.subarray(21).toString(encoding);
+    public static GetPayload(message: Buffer, encoding: BufferEncoding): string {
+        return message.subarray(13).toString(encoding);
     }
 
     public static Transaction = class {
-        public static GetChunkTxId(message: Buffer) {
-            return message.readUint32BE(17);
+        public static GetChunkTxId(message: Buffer): number {
+            return message.readUInt32BE(9);
         }
 
-        public static GetChunkPayload(message: Buffer, encoding: BufferEncoding = "utf8"): string {
+        public static GetChunkSeq(message: Buffer): number {
+            return message.readUInt32BE(13);
+        }
+
+        public static GetChunkPayload(message: Buffer, encoding: BufferEncoding): string {
+            return message.subarray(17).toString(encoding);
+        }
+
+        public static GetTxId(message: Buffer): number {
+            return message.readUInt32BE(13);
+        }
+
+        public static GetTxSize(message: Buffer): number {
+            return message.readInt32BE(17);
+        }
+
+        public static GetTxName(message: Buffer, encoding: "utf8" | "hex" = "utf8"): string {
             return message.subarray(21).toString(encoding);
         }
 
-        public static GetTxId(message: Buffer) {
-            return message.readUint32BE(21);
+        public static GetFlowBehaviour(message: Buffer): number {
+            return message.readUint8(13);
         }
 
-        public static GetTxName(message: Buffer, encoding: BufferEncoding = "utf8") {
-            return message.subarray(25).toString(encoding);
+        public static GetFetchTxId(message: Buffer): number {
+            return message.readUInt32BE(13);
+        }
+
+        public static GetFetchStart(message: Buffer): number {
+            return message.readUInt32BE(17);
+        }
+
+        public static GetFetchEnd(message: Buffer): number {
+            return message.readUInt32BE(21);
         }
     }
 }
--- src/web/BufferUtil.ts
+++ src/web/BufferUtil.ts
@@ -3,15 +3,11 @@ import {BinaryMessageType, BufferEncoding} from "../protocol_base.js";
 
 export class BufferUtil {
     public static GetType(message: CryoBuffer): BinaryMessageType {
-        const type = message.readUint8(16);
-        if (type > 0xff)
-            throw new Error(`Unable to decode type from message ${message}. MAX_TYPE = 255, got ${type} !`);
-
-        return type;
+        return message.readUint8(8);
     }
 
     public static GetAck(message: CryoBuffer): number {
-        return message.readUInt32BE(17);
+        return message.readUInt32BE(9);
     }
 
     public static GetSid(message: CryoBuffer): bigint {
@@ -19,24 +15,48 @@ export class BufferUtil {
     }
 
     public static GetPayload(message: CryoBuffer, encoding: BufferEncoding): string {
-        return message.subarray(21).toString(encoding);
+        return message.subarray(13).toString(encoding);
     }
 
     public static Transaction = class {
-        public static GetChunkTxId(message: CryoBuffer) {
-            return message.readUInt32BE(17);
+        public static GetChunkTxId(message: CryoBuffer): number {
+            return message.readUInt32BE(9);
+        }
+
+        public static GetChunkSeq(message: CryoBuffer): number {
+            return message.readUInt32BE(13);
         }
 
         public static GetChunkPayload(message: CryoBuffer, encoding: BufferEncoding): string {
+            return message.subarray(17).toString(encoding);
+        }
+
+        public static GetTxId(message: CryoBuffer): number {
+            return message.readUInt32BE(13);
+        }
+
+        public static GetTxSize(message: CryoBuffer): number {
+            return message.readInt32BE(17);
+        }
+
+        public static GetTxName(message: CryoBuffer, encoding: "utf8" | "hex" = "utf8"): string {
             return message.subarray(21).toString(encoding);
         }
 
-        public static GetTxId(message: CryoBuffer) {
-            return message.readUInt32BE(21);
+        public static GetFlowBehaviour(message: CryoBuffer): number {
+            return message.readUint8(13);
         }
 
-        public static GetTxName(message: CryoBuffer, encoding: "utf8" | "hex" = "utf8") {
-            return message.subarray(25).toString(encoding);
+        public static GetFetchTxId(message: CryoBuffer): number {
+            return message.readUInt32BE(13);
+        }
+
+        public static GetFetchStart(message: CryoBuffer): number {
+            return message.readUInt32BE(17);
+        }
+
+        public static GetFetchEnd(message: CryoBuffer): number {
+            return message.readUInt32BE(21);
         }
     }
 }
